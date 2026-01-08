6b87ae9775e3f2c07864d68ab4e31af655c6b606
Your Name <yashutandon56@gmail.com>
2026-01-08 14:57:56 +0530

    Add @mendable/firecrawl-js dependency and integrate firecrawl functionality in Inngest functions

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,40 @@
 import { generateText } from "ai";
 import { inngest } from "./client";
 import { google } from "@ai-sdk/google";
+import {firecrawl} from "@/lib/firecrawl"
 
+
+const URL_REGEX=/https?:\/\/[^\s]+/g;
 export const demoGenerate = inngest.createFunction(
   { id: "demo-generate" },
   { event: "demo/generate" },
-  async ({step }) => {
+  async ({event,step }) => {
+
+    const {prompt}=event.data as { prompt:string};
+
+    const urls=await step.run("exctract-urls",async ()=>{
+      return prompt.match(URL_REGEX) ?? [];
+    }) as string[];
+
+    const scrapedContent= await step.run("scrape-urls",async () =>{
+      const results=await Promise.all(
+        urls.map(async(url)=>{
+          if(!url) return null;
+          const result=await firecrawl.scrape(
+            url,
+            {formats:["markdown"]}
+          );
+          return result.markdown ?? null;
+        })
+      )
+      return results.filter(Boolean).join("\n\n");
+    })
+
+    const finalPrompt=scrapedContent ? `Context: \n${scrapedContent}\n\nQuestion:${prompt}`:prompt;
     await step.run("generate-text",async () => {
         return await generateText({
             model:google("gemini-2.5-flash"),
-            prompt: 'Write a chole bhature recipe for 4 people.',
+            prompt: finalPrompt,
         })
     })
   
